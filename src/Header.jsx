a2c34e737ec595f4bd099d6941c937ad60b4f07d
kea-S <keaharvan1@gmail.com>
2024-06-13 19:33:52 +0800

    added header

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='header'>
+        <h2 className='header--title'>Header component</h2>
+        <h4 className='header--project'>React Course - Project 3</h4>
+    </header>
+  )
+}
+
+export default Header
